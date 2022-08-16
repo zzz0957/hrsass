cdc0c2ff99f2c46a33da522b2aeffa222eade6d8
zxy <1378745457@qq.com>
2022-08-16 15:09:56 +0800

    人资项目初始化

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+// 些项目信息的配置
+module.exports = {
+
+  title: 'Vue Admin Template', // 项目名称
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false, // 固定头部
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false // 显示左侧菜单logo
+}
---
+++ src/store/modules/user.js
@@ -0,0 +1,10 @@
+// 用户信息
+
+export default {
+  // 如果希望你的模块具有更高的封装度和复用性，你可以通过添加 namespaced: true 的方式使其成为命名空间模块。当模块被注册后，它的所有 getter、action 及 mutation 都会自动根据模块注册的路径调整命名。
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {}
+}
+
